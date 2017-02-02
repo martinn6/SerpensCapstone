3afcbb705980db1f0b95901ba88e7bf56554b282
nickdmartin <nickdmartin@outlook.com>
2017-02-01 19:35:15 -0800

    Testing SQL Connection.

--- SQLTest.php
+++ SQLTest.php
@@ -8,23 +8,13 @@
 							   "PWD" => "T3amSerpin$!");
 							   
 	echo "Trying to connect...\n";
-	$connection = sqlsrv_connect($serverName, $connectionOptions);
+	$conn = sqlsrv_connect($serverName, $connectionOptions);
 	 
-	if($connection === false)
+	if($conn === false)
 	{
 		die(print_r(sqlsrv_errors(), true));
 	}
-		
-	if ($connection)
-	{
-		$res= mssql_query('SELECT * FROM [OSU_Capstone].[dbo].[Awards]', $connection);
-        print_r(mssql_get_last_message());
-        $row = mssql_fetch_array($res);
-
-        echo $row[0];
-
-	}	
-
+			
 
 	?>
 </BODY>
