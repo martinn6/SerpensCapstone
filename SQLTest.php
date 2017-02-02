58414ff71a75fe334aed707aa6d3281f51b103b6
nickdmartin <nickdmartin@outlook.com>
2017-02-01 19:40:39 -0800

    connection qa.

--- SQLTest.php
+++ SQLTest.php
@@ -6,12 +6,20 @@
 	 $user = "Serpins_Login";
 	 $pwd = "T3amSerpin$!";
 	 $db = "OSU_Capstone";
+	 echo "Trying to connect....\n";
 	 try{
 		 $conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
 	  }
 	 catch(Exception $e){
 		 die(print_r($e));
 	 }
+	 
+	 if($conn)
+	 {
+		 echo "Connection Established.\n";
+	 }
+	 
+	 
  echo "Done.\n";
  ?>
 </BODY>
