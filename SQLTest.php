4cd05efa8236eb6df1be5890ea3f689de4b5a606
nickdmartin <nickdmartin@outlook.com>
2017-02-01 19:39:14 -0800

    sql test

--- SQLTest.php
+++ SQLTest.php
@@ -1,21 +1,18 @@
 <HTML>
 <BODY>
 	<?php
-
-	$serverName = "tcp:cs496osusql.database.windows.net, 1433";
-	$connectionOptions = array("Database" => "OSU_Capstone", 
-							   "UID" => "Serpins_Login",
-							   "PWD" => "T3amSerpin$!");
-							   
-	echo "Trying to connect...\n";
-	$conn = sqlsrv_connect($serverName, $connectionOptions);
-	 
-	if($conn === false)
-	{
-		die(print_r(sqlsrv_errors(), true));
-	}
-			
-
-	?>
+	 // DB connection info
+	 $host = "cs496osusql.database.windows.net";
+	 $user = "Serpins_Login";
+	 $pwd = "T3amSerpin$!";
+	 $db = "OSU_Capstone";
+	 try{
+		 $conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
+	  }
+	 catch(Exception $e){
+		 die(print_r($e));
+	 }
+ echo "Done.\n";
+ ?>
 </BODY>
 </HTML>
