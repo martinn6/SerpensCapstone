40d1492fb5e0c164a4bd374e16261d8441dbc3ae
nickdmartin <nickdmartin@outlook.com>
2017-02-01 18:30:32 -0800

    Adding PDFTest.php

---
+++ PDFTest.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+require('fpdf.php');
+$pdf = new FPDF();
+$pdf->AddPage();
+$pdf->SetFont('Arial','B',16);
+$pdf->Cell(40,10,'Hello World!');
+$pdf->Output();
+?>
+
+</body>
+</html> 
