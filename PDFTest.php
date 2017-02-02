cff2be0bc4231a1c0ffce04a14be68f812c5f295
nickdmartin <nickdmartin@outlook.com>
2017-02-01 19:03:56 -0800

    Adding fdpf. Modifying PDFTest.

--- PDFTest.php
+++ PDFTest.php
@@ -1,15 +1,9 @@
-<!DOCTYPE html>
-<html>
-<body>
 
 <?php
-require('fpdf.php');
-$pdf = new FPDF();
+require_once( "fpdf/fpdf.php" );
+$pdf = new FPDF( 'P', 'mm', 'A4' );
 $pdf->AddPage();
 $pdf->SetFont('Arial','B',16);
 $pdf->Cell(40,10,'Hello World!');
-$pdf->Output();
+$pdf->Output("pdftest.pdf","I");
 ?>
-
-</body>
-</html> 
