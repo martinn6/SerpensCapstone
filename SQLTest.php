c99fae43e50b62188d6f1402c89ea6cae1d5f4bc
nickdmartin <nickdmartin@outlook.com>
2017-02-01 19:20:04 -0800

    adding port to server.

--- SQLTest.php
+++ SQLTest.php
@@ -1,25 +1,26 @@
 <HTML>
 <BODY>
-<?php
+	<?php
 
-$server = "cs496osusql.database.windows.net";
-$username = "Serpins_Login";
-$password = "T3amSerpin$!";
-$database = "OSU_Capstone";
-try
-{
-	echo "Trying Connection...";
-    $conn = new PDO("sqlsrv:server=$server ; Database = $database", $username, $password);
-}
-catch(Exception $e)
-{
-	echo "Connection Failed.";
-    die(print_r($e));
-}
+	$server = "cs496osusql.database.windows.net,1433";
+	$username = "Serpins_Login";
+	$password = "T3amSerpin$!";
+	$database = "OSU_Capstone";
+	
+	try
+	{
+		echo "Trying Connection to $server...";
+		$conn = new PDO("sqlsrv:server=$server ; Database = $database", $username, $password);
+	}
+	catch(Exception $e)
+	{
+		echo "Connection Failed.";
+		die(print_r($e));
+	}
 
-echo "Done.";
+	echo "Done.";
 
 
-?>
+	?>
 </BODY>
 </HTML>
