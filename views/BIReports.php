86154af4af530ee2b4fce1ab832eaf698bdb7296
Maxwell Burdick <Maxwell Burdick>
2017-02-02 18:46:46 -0600

    fasd

--- views/BIReports.php
+++ views/BIReports.php
@@ -41,39 +41,7 @@
 	</div>
 </div>
 <?php
-	// DB connection info
-	$host = "cs496osusql.database.windows.net";
-	$user = "Serpins_Login";
-	$pwd = "T3amSerpin$!";
-	$db = "OSU_Capstone";
-	echo "<p>Trying to connect....</p>";
-	try{
-		$conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
-	}
-	catch(Exception $e){
-		die(print_r($e));
-	}
-	
-	if($conn)
-	{
-		echo "<p>Connection Established.</p>";
-		// fetch mysql table rows
-		$sql = "SELECT * FROM dbo.Awards";
-		$sqlResultesult = sqlsrv_query($conn, $sql);
-		$csvName = "export.csv"
-		$fp = fopen(csvName , 'w');
-		echo "opened";
-		while ($export = odbc_fetch_array($sqlRresult)) {
-			if (!isset($headings))
-			{
-				$headings = array_keys($export);
-				fputcsv($fp, $headings, ',', '"');
-			}
-			fputcsv($fp, $export, ',', '"');
-		}
-		fclose($fp);
-		echo "<p>success</p>";
-	}
+	include('../php/csvTest.php');
 ?>
 
 <script src="../js/jquery-2.1.4.min.js"></script>
