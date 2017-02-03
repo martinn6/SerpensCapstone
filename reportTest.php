a3f69a94f6df1e9a194d6f3a27701052a21d8edb
Maxwell Burdick <Maxwell Burdick>
2017-02-02 19:01:10 -0600

    fasd

--- reportTest.php
+++ reportTest.php
@@ -1,12 +1,11 @@
-<!DOCTYPE html>
 <html lang="en">
 <head>
-  <!--<meta charset="UTF-8">
+  <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="../css/bootstrap.min.css">
   <link rel="stylesheet" href="../css/styles.css">
-  <title>BI Reports</title>-->
+  <title>BI Reports</title>
 </head>
 <body>
 <script src="../js/jquery-2.1.4.min.js"></script>
