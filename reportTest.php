e8990548ba7bcddf92e0714101509be2eac1d03e
Maxwell Burdick <Maxwell Burdick>
2017-02-02 18:54:21 -0600

    fdas

--- reportTest.php
+++ reportTest.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+<!--<head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>BI Reports</title>
-</head>
+</head>-->
 <body>
 <?php
     echo"<p>Start</p>";
