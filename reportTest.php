90490bfa6b3521fdca89eb9f735f58b9d53f71ed
Maxwell Burdick <Maxwell Burdick>
2017-02-02 18:50:44 -0600

    fasd

--- reportTest.php
+++ reportTest.php
@@ -4,12 +4,44 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="../css/bootstrap.min.css">
-  <link rel="stylesheet" href="../css/styles.css">
   <title>BI Reports</title>
 </head>
 <body>
-
+<?php
+	// DB connection info
+	$host = "cs496osusql.database.windows.net";
+	 $user = "Serpins_Login";
+	 $pwd = "T3amSerpin$!";
+	 $db = "OSU_Capstone";
+	 echo "<p>Trying to connect....</p>";
+	 try{
+		 $conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
+	  }
+	 catch(Exception $e){
+		 die(print_r($e));
+	 }
+	
+	if($conn)
+	{
+		echo "<p>Connection Established.</p>";
+		// fetch mysql table rows
+		$sql = "SELECT * FROM dbo.Awards";
+		$sqlResultesult = sqlsrv_query($conn, $sql);
+		$csvName = "export.csv"
+		$fp = fopen(csvName , 'w');
+		echo "opened";
+		while ($export = odbc_fetch_array($sqlRresult)) {
+			if (!isset($headings))
+			{
+				$headings = array_keys($export);
+				fputcsv($fp, $headings, ',', '"');
+			}
+			fputcsv($fp, $export, ',', '"');
+		}
+		fclose($fp);
+		echo "<p>success</p>";
+	}
+?>
 <div class="container">
 	<div class="row">
 		<section class="col-xs-offset-3 col-xs-6">
@@ -40,41 +72,6 @@
 		</section>
 	</div>
 </div>
-<?php
-	// DB connection info
-	$host = "cs496osusql.database.windows.net";
-	$user = "Serpins_Login";
-	$pwd = "T3amSerpin$!";
-	$db = "OSU_Capstone";
-	echo "<p>Trying to connect....</p>";
-	try{
-		$conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
-	}
-	catch(Exception $e){
-		die(print_r($e));
-	}
-	
-	if($conn)
-	{
-		echo "<p>Connection Established.</p>";
-		// fetch mysql table rows
-		$sql = "SELECT * FROM dbo.Awards";
-		$sqlResultesult = sqlsrv_query($conn, $sql);
-		$csvName = "export.csv"
-		$fp = fopen(csvName , 'w');
-		echo "opened";
-		while ($export = odbc_fetch_array($sqlRresult)) {
-			if (!isset($headings))
-			{
-				$headings = array_keys($export);
-				fputcsv($fp, $headings, ',', '"');
-			}
-			fputcsv($fp, $export, ',', '"');
-		}
-		fclose($fp);
-		echo "<p>success</p>";
-	}
-?>
 
 <script src="../js/jquery-2.1.4.min.js"></script>
 <script src="../js/bootstrap.min.js"></script>
