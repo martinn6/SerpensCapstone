9438e0f0a7401dbd2fcf3a3a16582cc4a4bddda3
Maxwell Burdick <Maxwell Burdick>
2017-02-02 18:55:17 -0600

    afsd

--- reportTest.php
+++ reportTest.php
@@ -7,41 +7,44 @@
   <title>BI Reports</title>
 </head>-->
 <body>
+<script src="../js/jquery-2.1.4.min.js"></script>
+<script src="../js/bootstrap.min.js"></script>
+<script src="../js/script.js"></script>
 <?php
     echo"<p>Start</p>";
-	// DB connection info
-	// $host = "cs496osusql.database.windows.net";
-	//  $user = "Serpins_Login";
-	//  $pwd = "T3amSerpin$!";
-	//  $db = "OSU_Capstone";
-	//  echo "<p>Trying to connect....</p>";
-	//  try{
-	// 	 $conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
-	//   }
-	//  catch(Exception $e){
-	// 	 die(print_r($e));
-	//  }
+	DB connection info
+	$host = "cs496osusql.database.windows.net";
+	 $user = "Serpins_Login";
+	 $pwd = "T3amSerpin$!";
+	 $db = "OSU_Capstone";
+	 echo "<p>Trying to connect....</p>";
+	 try{
+		 $conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
+	  }
+	 catch(Exception $e){
+		 die(print_r($e));
+	 }
 	
-	// if($conn)
-	// {
-	// 	echo "<p>Connection Established.</p>";
-	// 	// fetch mysql table rows
-	// 	$sql = "SELECT * FROM dbo.Awards";
-	// 	$sqlResultesult = sqlsrv_query($conn, $sql);
-	// 	$csvName = "export.csv"
-	// 	$fp = fopen(csvName , 'w');
-	// 	echo "opened";
-	// 	while ($export = odbc_fetch_array($sqlRresult)) {
-	// 		if (!isset($headings))
-	// 		{
-	// 			$headings = array_keys($export);
-	// 			fputcsv($fp, $headings, ',', '"');
-	// 		}
-	// 		fputcsv($fp, $export, ',', '"');
-	// 	}
-	// 	fclose($fp);
-	// 	echo "<p>success</p>";
-	// }
+	if($conn)
+	{
+		echo "<p>Connection Established.</p>";
+		// fetch mysql table rows
+		$sql = "SELECT * FROM dbo.Awards";
+		$sqlResultesult = sqlsrv_query($conn, $sql);
+		$csvName = "export.csv"
+		$fp = fopen(csvName , 'w');
+		echo "opened";
+		while ($export = odbc_fetch_array($sqlRresult)) {
+			if (!isset($headings))
+			{
+				$headings = array_keys($export);
+				fputcsv($fp, $headings, ',', '"');
+			}
+			fputcsv($fp, $export, ',', '"');
+		}
+		fclose($fp);
+		echo "<p>success</p>";
+	}
 ?>
 <div class="container">
 	<div class="row">
@@ -74,8 +77,6 @@
 	</div>
 </div>
 
-<script src="../js/jquery-2.1.4.min.js"></script>
-<script src="../js/bootstrap.min.js"></script>
-<script src="../js/script.js"></script>
+
 </body>
 </html>
