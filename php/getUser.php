f7ef3d9a9e6fe10f601331009f96bf9792e5457b
Maxwell Burdick <Maxwell Burdick>
2017-02-08 14:34:21 -0600

    git

--- php/getUser.php
+++ php/getUser.php
@@ -2,43 +2,25 @@
 
 $json = json_decode($_Post[]);
 echo $json;
-$email = $_POST['email'];
 
-    // DB connection info
-    $host = "cs496osusql.database.windows.net";
-    $user = "Serpins_Login";
-    $pwd = "T3amSerpin$!";
-    $db = "OSU_Capstone";
-    echo "<p>Trying to connect....</p>";
-    try{
-        $conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
-    }
-    catch(Exception $e){
-        die(print_r($e));
-    }
+//     // DB connection info
+//     $host = "cs496osusql.database.windows.net";
+//     $user = "Serpins_Login";
+//     $pwd = "T3amSerpin$!";
+//     $db = "OSU_Capstone";
+//     echo "<p>Trying to connect....</p>";
+//     try{
+//         $conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
+//     }
+//     catch(Exception $e){
+//         die(print_r($e));
+//     }
     
-    if($conn)
-    {
-        echo "<p>Connection Established.</p>";
-        $sql_select = "SELECT * FROM dbo.User where email = testEmail";
-        $stmt = $conn->query($sql_select);
-        $awards = $stmt->fetchAll();
-        if(count($awards) > 0) {
-            echo "<h2>Awards:</h2>";
-            echo "<table>";
-            echo "<tr><th>AwardId</th>";
-            echo "<th>Award Name</th></tr>";
-        foreach($awards as $award) {
-            echo "<tr><td>".$award['AwardId']."</td>";
-            echo "<td>".$award['AwardTypeName']."</td></tr>";
-        }
-        echo "</table>";
-    }	
-    else {
-        echo "<p>No awards found.</p>";
-    } 
-    }
+//     if($conn)
+//     {
+      
+//     }
     
     
-echo "<p>Done.</p>";
+// echo "<p>Done.</p>";
 ?>
