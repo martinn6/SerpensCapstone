5f5440bdd8c55144f50d3c7b16673855ec90cd10
nickdmartin <nickdmartin@outlook.com>
2017-02-07 19:28:38 -0800

    Updated sendgrid code.

--- EmailTest.php
+++ EmailTest.php
@@ -3,14 +3,23 @@
 <BODY>
 <?php
 	echo "Email Test Start.";
-	$sendgrid = new SendGrid('serpenscapstone', 'T3amSerpin$!');
-	$mail = new SendGridMail();
-	$mail->addTo('martinn6@oregonstate.edu')->
-		   setFrom('serpenscapstone@gmail.com')->
-		   setSubject('Email Test')->
-		   setText('Hello World!')->
-		   setHtml('<strong>Hello World!</strong>');
-	$sendgrid->smtp->send($mail);
+	// using SendGrid's PHP Library
+	// https://github.com/sendgrid/sendgrid-php
+
+	$from = new SendGrid\Email("Example User", "test@example.com");
+	$subject = "Sending with SendGrid is Fun";
+	$to = new SendGrid\Email("Nick Martin", "martinn6@oregonstate.edu");
+	$content = new SendGrid\Content("text/plain", "and easy to do anywhere, even with PHP");
+	$mail = new SendGrid\Mail($from, $subject, $to, $content);
+
+	$apiKey = "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";
+	$sg = new \SendGrid($apiKey);
+
+	$response = $sg->client->mail()->send()->post($mail);
+	echo $response->statusCode();
+	echo $response->headers();
+	echo $response->body();
+	
 	echo "Email Test End.";
 ?>
 </BODY>
