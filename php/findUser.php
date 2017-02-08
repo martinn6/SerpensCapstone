b15a96c960346d1b3228c6c3e85f389707c66f71
Maxwell Burdick <Maxwell Burdick>
2017-02-08 15:56:27 -0600

    adfs

--- php/findUser.php
+++ php/findUser.php
@@ -1,31 +1,30 @@
 <?php
 
-$data = file_get_contents("php://input");
-echo $data;
-echo "\r\n";
-$_POST = json_decode(file_get_contents('php://input'), true);
-echo $_POST;
 $email = $_POST["email"];
 echo $email;
 
-//     // DB connection info
-//     $host = "cs496osusql.database.windows.net";
-//     $user = "Serpins_Login";
-//     $pwd = "T3amSerpin$!";
-//     $db = "OSU_Capstone";
-//     echo "<p>Trying to connect....</p>";
-//     try{
-//         $conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
-//     }
-//     catch(Exception $e){
-//         die(print_r($e));
-//     }
+    // DB connection info
+    $host = "cs496osusql.database.windows.net";
+    $user = "Serpins_Login";
+    $pwd = "T3amSerpin$!";
+    $db = "OSU_Capstone";
+    echo "/r/nTrying to connect..../r/n";
+    try{
+        $conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
+    }
+    catch(Exception $e){
+        die(print_r($e));
+        echo "/r/nCONNECTION ERROR/r/n";
+    }
     
-//     if($conn)
-//     {
-      
-//     }
+    if($conn)
+    {
+      $sql_select = "SELECT * FROM dbo.UserAccount where email = $email";
+		 $stmt = $conn->query($sql_select);
+		 $user = $stmt->fetchAll();
+         echo $user;
+    }
     
     
-// echo "<p>Done.</p>";
+echo "Done.";
 ?>
