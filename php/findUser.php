abb7378b924a5490b26e458d9a647705f8ba6bb8
Maxwell Burdick <Maxwell Burdick>
2017-02-08 16:11:01 -0600

    fas

--- php/findUser.php
+++ php/findUser.php
@@ -1,32 +1,28 @@
 <?php
 
 $email = $_POST["email"];
-echo $email;
 
     // DB connection info
     $host = "cs496osusql.database.windows.net";
     $user = "Serpins_Login";
     $pwd = "T3amSerpin$!";
     $db = "OSU_Capstone";
-    echo "\r\n Trying to connect....\r\n";
     try{
         $conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
     }
     catch(Exception $e){
-        echo "\r\n CONNECTION ERROR\r\n";
         die(print_r($e));
     }
     
     if($conn)
     {
-        echo "success\r\n";
-      $sql_select = "SELECT * FROM dbo.UserAccount where email = '".$email."'";
-		 $stmt = $conn->query($sql_select);
-		 $user = $stmt->fetchAll();
-         $result = json_encode($user);
-         echo $result;
+        $sql_select = "SELECT * FROM dbo.UserAccount where Email = '".$email."'";
+        $stmt = $conn->query($sql_select);
+        $result = $stmt->fetchAll();
+        if ($result['FullName'] === NULL) {
+            return false;
+        } else {
+            return $result
+        }
     }
-    
-    
-echo "\r\n Done.";
 ?>
