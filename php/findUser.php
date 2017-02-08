480d41d8455fbc2e87f2de03fa3d3b43dc76dcc7
Maxwell Burdick <Maxwell Burdick>
2017-02-08 16:03:08 -0600

    afsd

--- php/findUser.php
+++ php/findUser.php
@@ -8,18 +8,18 @@ echo $email;
     $user = "Serpins_Login";
     $pwd = "T3amSerpin$!";
     $db = "OSU_Capstone";
-    echo "/r/nTrying to connect..../r/n";
+    echo "\r\n Trying to connect....\r\n";
     try{
         $conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
     }
     catch(Exception $e){
+        echo "\r\n CONNECTION ERROR\r\n";
         die(print_r($e));
-        echo "/r/nCONNECTION ERROR/r/n";
     }
     
     if($conn)
     {
-      $sql_select = "SELECT * FROM dbo.UserAccount where email = $email";
+      $sql_select = "SELECT * FROM dbo.UserAccount where email = '".$email."'";
 		 $stmt = $conn->query($sql_select);
 		 $user = $stmt->fetchAll();
          echo $user;
