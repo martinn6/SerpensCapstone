3fbe77d6ee68210d95a090a6c84f555c7895681c
nickdmartin <nickdmartin@outlook.com>
2017-02-08 22:33:14 -0800

    email v.1.23

--- EmailTest.php
+++ EmailTest.php
@@ -2,7 +2,7 @@
 <HTML>
 <BODY>
 <?php
-	echo "Email test start...V1.22<BR>";
+	echo "Email test start...V1.23<BR>";
 	
 	require '/email/PHPMailerAutoload.php';
 
@@ -11,22 +11,15 @@
 	//$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 	$mail->isSMTP();                                      // Set mailer to use SMTP
-	$mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
+	$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 	$mail->SMTPAuth = true;                               // Enable SMTP authentication
-	$mail->Username = 'user@example.com';                 // SMTP username
-	$mail->Password = 'secret';                           // SMTP password
-	$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+	$mail->Username = 'serpenscapstone@gmail.com' ;                // SMTP username
+	$mail->Password = 'T3amSerpin$!';                           // SMTP password
+	//$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 	$mail->Port = 587;                                    // TCP port to connect to
 
-	$mail->setFrom('from@example.com', 'Mailer');
+	$mail->setFrom('serpenscapstone@gmail.com', 'Serpens');
 	$mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
-	$mail->addAddress('ellen@example.com');               // Name is optional
-	$mail->addReplyTo('info@example.com', 'Information');
-	$mail->addCC('cc@example.com');
-	$mail->addBCC('bcc@example.com');
-
-	$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-	$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 	$mail->isHTML(true);                                  // Set email format to HTML
 
 	$mail->Subject = 'Here is the subject';
