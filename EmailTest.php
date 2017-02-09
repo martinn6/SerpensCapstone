fb932591d83f368e917c3f6827c4dbba53265f69
nickdmartin <nickdmartin@outlook.com>
2017-02-08 21:46:22 -0800

    Updating email test

--- EmailTest.php
+++ EmailTest.php
@@ -4,7 +4,34 @@
 <?php
 	echo "Email test start...<BR>";
 	
-	 
+	 require_once "Mail.php";
+ 
+	$from = "Serpens <serpenscapstone@gmail.com>";
+	$to = "Nick Martin <martinn6@oregonstate.edu>";
+	$subject = "Hi!";
+	$body = "Hi,\n\nHow are you?";
+
+	$host = "smtp.gmail.com";
+	$username = "serpenscapstone";
+	$password = "T3amSerpin$!";
+
+	$headers = array ('From' => $from,
+	  'To' => $to,
+	  'Subject' => $subject);
+	$smtp = Mail::factory('smtp',
+	  array ('host' => $host,
+	    'auth' => true,
+	    'username' => $username,
+	    'password' => $password));
+
+	$mail = $smtp->send($to, $headers, $body);
+
+	if (PEAR::isError($mail)) {
+	  echo("<p>" . $mail->getMessage() . "</p>");
+	 } else {
+	  echo("<p>Message successfully sent!</p>");
+	 }
+
 	
 	echo "Email test end.<BR>";
 ?>
