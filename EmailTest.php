4a96195791eee2e9c6b41d9a0a5cf08e8fb1b23d
nickdmartin <nickdmartin@outlook.com>
2017-02-08 21:48:58 -0800

    email v1.12

--- EmailTest.php
+++ EmailTest.php
@@ -2,35 +2,35 @@
 <HTML>
 <BODY>
 <?php
-	echo "Email test start...V1.11<BR>";
+	echo "Email test start...V1.12<BR>";
 	
 	 require_once "Mail.php";
  
-	$from = "Serpens <serpenscapstone@gmail.com>";
-	$to = "Nick Martin <martinn6@oregonstate.edu>";
-	$subject = "Hi!";
-	$body = "Hi,\n\nHow are you?";
+	// $from = "Serpens <serpenscapstone@gmail.com>";
+	// $to = "Nick Martin <martinn6@oregonstate.edu>";
+	// $subject = "Hi!";
+	// $body = "Hi,\n\nHow are you?";
 
-	$host = "smtp.gmail.com";
-	$username = "serpenscapstone";
-	$password = "T3amSerpin$!";
+	// $host = "smtp.gmail.com";
+	// $username = "serpenscapstone";
+	// $password = "T3amSerpin$!";
 
-	$headers = array ('From' => $from,
-	  'To' => $to,
-	  'Subject' => $subject);
-	$smtp = Mail::factory('smtp',
-	  array ('host' => $host,
-	    'auth' => true,
-	    'username' => $username,
-	    'password' => $password));
+	// $headers = array ('From' => $from,
+	//   'To' => $to,
+	//   'Subject' => $subject);
+	// $smtp = Mail::factory('smtp',
+	//   array ('host' => $host,
+	//     'auth' => true,
+	//     'username' => $username,
+	//     'password' => $password));
 
-	$mail = $smtp->send($to, $headers, $body);
+	// $mail = $smtp->send($to, $headers, $body);
 
-	if (PEAR::isError($mail)) {
-	  echo("<p>" . $mail->getMessage() . "</p>");
-	 } else {
-	  echo("<p>Message successfully sent!</p>");
-	 }
+	// if (PEAR::isError($mail)) {
+	//   echo("<p>" . $mail->getMessage() . "</p>");
+	//  } else {
+	//   echo("<p>Message successfully sent!</p>");
+	//  }
 
 	
 	echo "Email test end.<BR>";
