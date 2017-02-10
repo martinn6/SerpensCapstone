3ab6c6dd1d4a5176d72571c3c2f8cb659fd20701
Maxwell Burdick <Maxwell Burdick>
2017-02-09 19:14:27 -0600

    a

--- php/addUser.php
+++ php/addUser.php
@@ -15,15 +15,14 @@ $name = $_POST["fName"] $_POST["lName"];
     catch(Exception $e){
         die(print_r($e));
     }
-    if(!($stmt = $conn->prepare("INSERT INTO UserAccount(Email,Password,FullName,UserTypeID)
-    values(:em,:pw,:fn (SELECT id from UserType where UserType.id=:ut))"))){
+    if(!($stmt = $conn->prepare("INSERT INTO dbo.UserAccount(Email,Password,FullName,UserTypeID)
+    values(:em,:pw,:fn (SELECT id FROM dbo.UserType WHERE id=Admin))"))){
         die(print_r($stmt->errorInfo()));
     } 
     if(!$stmt->execute(array(
         ':em' => $email,
         ':pw' => $password,
         ':fn' => $name,
-        ':ut' => 'Admin'
     ))){
         if($stmt->errorInfo() == 1062){
         die(printf("Cannot add '" .$name. "' because there is already a user with the email '".$email."'."));
