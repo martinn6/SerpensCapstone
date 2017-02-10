a8ed02fceb28e809142d094eab9e0af1721116e4
nickdmartin <nickdmartin@outlook.com>
2017-02-09 17:18:16 -0800

    Email update v.1.24

--- EmailTest.php
+++ EmailTest.php
@@ -2,38 +2,77 @@
 <HTML>
 <BODY>
 <?php
-	echo "Email test start...V1.23<BR>";
+	echo "Email Test v.1.24"
 	
-	require '/email/PHPMailerAutoload.php';
+	/**
+	 * This example shows settings to use when sending via Google's Gmail servers.
+	 */
 
+	//SMTP needs accurate times, and the PHP time zone MUST be set
+	//This should be done in your php.ini, but this is how to do it if you don't have access to that
+	date_default_timezone_set('Etc/UTC');
+
+	require '/PHPMailer/PHPMailerAutoload.php';
+
+	//Create a new PHPMailer instance
 	$mail = new PHPMailer;
 
-	//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+	//Tell PHPMailer to use SMTP
+	$mail->isSMTP();
 
-	$mail->isSMTP();                                      // Set mailer to use SMTP
-	$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
-	$mail->SMTPAuth = true;                               // Enable SMTP authentication
-	$mail->Username = 'serpenscapstone@gmail.com' ;                // SMTP username
-	$mail->Password = 'T3amSerpin$!';                           // SMTP password
-	//$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-	$mail->Port = 587;                                    // TCP port to connect to
+	//Enable SMTP debugging
+	// 0 = off (for production use)
+	// 1 = client messages
+	// 2 = client and server messages
+	$mail->SMTPDebug = 2;
 
-	$mail->setFrom('serpenscapstone@gmail.com', 'Serpens');
-	$mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
-	$mail->isHTML(true);                                  // Set email format to HTML
+	//Ask for HTML-friendly debug output
+	$mail->Debugoutput = 'html';
 
-	$mail->Subject = 'Here is the subject';
-	$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-	$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+	//Set the hostname of the mail server
+	$mail->Host = 'smtp.gmail.com';
+	// use
+	// $mail->Host = gethostbyname('smtp.gmail.com');
+	// if your network does not support SMTP over IPv6
 
-	if(!$mail->send()) {
-		echo 'Message could not be sent.';
-		echo 'Mailer Error: ' . $mail->ErrorInfo;
+	//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+	$mail->Port = 587;
+
+	//Set the encryption system to use - ssl (deprecated) or tls
+	$mail->SMTPSecure = 'tls';
+
+	//Whether to use SMTP authentication
+	$mail->SMTPAuth = true;
+
+	//Username to use for SMTP authentication - use full email address for gmail
+	$mail->Username = "serpentscapstone@gmail.com";
+
+	//Password to use for SMTP authentication
+	$mail->Password = "T3amSerpin$!";
+
+	//Set who the message is to be sent from
+	$mail->setFrom('serpentscapstone@gmail.com', 'serpents');
+
+	//Set who the message is to be sent to
+	$mail->addAddress('martinn6@oregonstate.edu', 'Nick Martin');
+
+	//Set the subject line
+	$mail->Subject = 'PHPMailer GMail SMTP test';
+
+	//Read an HTML message body from an external file, convert referenced images to embedded,
+	//convert HTML into a basic plain-text alternative body
+	$mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
+
+	//Replace the plain text body with one created manually
+	$mail->AltBody = 'This is a plain-text message body';
+
+	//send the message, check for errors
+	if (!$mail->send()) {
+		echo "Mailer Error: " . $mail->ErrorInfo;
 	} else {
-		echo 'Message has been sent';
+		echo "Message sent!";
 	}
-	
-	echo "Email test end.<BR>";
+
 ?>
 </BODY>
 </HTML>
