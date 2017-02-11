4e4b7502984775a2a5803addc197fd6c967de578
Maxwell Burdick <Maxwell Burdick>
2017-02-11 09:48:03 -0600

    a

--- php/editUser.php
+++ php/editUser.php
@@ -19,6 +19,7 @@ $email = $_POST["email"];
         try {
             $stmt->execute(array('email' => $email));
             $result=$stmt->fetchAll(PDO::FETCH_ASSOC);
+            print_r($result);
             $name = $result['FullName'];
             print_r($name);
         } catch (PDOException $e) {
