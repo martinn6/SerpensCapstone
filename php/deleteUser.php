640b869abc8d65b945df0bbfec026ab4a72bad5c
Maxwell Burdick <Maxwell Burdick>
2017-02-11 08:49:08 -0600

    a

--- php/deleteUser.php
+++ php/deleteUser.php
@@ -31,7 +31,7 @@ $email = $_POST["email"];
         $stmt = $conn->prepare('DELETE * FROM dbo.UserAccount WHERE email = :email');
         try {
             $stmt->execute(array('email' => $email));
-            $printf("Deleted '" .$name. "' from the database. ");
+            printf("Deleted '" .$name. "' from the database. ");
         } catch (PDOException $e) {
             if ($e->errorInfo[1] == 1054) {
                 printf("Cannot find user with email '" .$email. "'.");
