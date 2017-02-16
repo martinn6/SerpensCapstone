c158d37319666e7bed605354e76c31f0c8203e8d
Maxwell Burdick <Maxwell Burdick>
2017-02-15 19:13:37 -0600

    edit

--- admin/adminLogin.php
+++ admin/adminLogin.php
@@ -46,10 +46,10 @@ if (isset($_POST['submit'])){
   <link rel="stylesheet" href="../css/styles.css">
   </head>
 <body>
-<script>
 <script src="../js/jquery-2.1.4.min.js"></script>
 <script src="../js/bootstrap.min.js"></script>
 <script src="../js/script.js"></script>
+<script>
 function isEmail(email) {
 	var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 	return regex.test(email);
