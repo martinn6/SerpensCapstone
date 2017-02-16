866c1ec8ec2b50eacc65d1a08b78b89ee22490a6
nickdmartin <nickdmartin@outlook.com>
2017-02-15 21:48:26 -0800

    pdf generation v1.2

--- PDFTest.php
+++ PDFTest.php
@@ -1,10 +1,15 @@
 
 <?php
+
+$version = '1.2';
+
 require_once( "fpdf/fpdf.php" );
 $pdf = new FPDF( 'L', 'mm', 'Letter' );
 $pdf->AddPage();
 $pdf->Image( "images/AwardBackground.png", 0, 0, 280 );
-$pdf->SetFont('Arial','B',32);
-$pdf->Cell(120,40,'Employee of the Year! v1.1');
+$pdf->SetFont('Arial','B',48);
+$pdf->Cell(60,60,'Employee of the Year');
+$pdf->SetFont('Arial','B', 8);
+$pdf->Cell(200,200, $version);
 $pdf->Output("pdftest.pdf","I");
 ?>
