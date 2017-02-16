2f84c4b42adfdbab4b4d8b5bf00a61b90ccd77c1
Maxwell Burdick <coffeehorn@gmail.com>
2017-02-16 10:28:49 -0600

    fds

--- admin/editUser.php
+++ admin/editUser.php
@@ -6,7 +6,7 @@ if(!isset($_SESSION['admin'])){
 	header("Location: ../php/adminLogout.php"); 
 	die();
 } 
-if(!isset($_SESSION['editUserEmail'] && $_SESSION['editUserName'])){
+if(!isset($_SESSION['editUserEmail']) && !isset($_SESSION['editUserName'])){
 	header("Location: ../php/admin.php"); 
 	die();
 } 
