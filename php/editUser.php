a607908de661d2ce057594ebf67bf344ce8abc97
Maxwell Burdick <coffeehorn@gmail.com>
2017-02-16 09:46:49 -0600

    a

--- php/editUser.php
+++ php/editUser.php
@@ -6,7 +6,6 @@ $cred_match = false;
 
 if(!empty($_POST)){
 	if ($conn){
-        	$err_msg = "conn ";
 		$query = "SELECT * FROM dbo.UserAccount WHERE Email = :Email";
 		$query_params = array(':Email' => $_POST['email']);
 		$stmt = $conn->prepare($query);
@@ -14,7 +13,6 @@ if(!empty($_POST)){
 		$row = $stmt->fetch();
         
         	if($row){
-
         		if ($cred_match){
             			if (session_status() == PHP_SESSION_NONE) {
                 			session_start();
@@ -26,29 +24,11 @@ if(!empty($_POST)){
 		} else {
 			$err_msg = "Cannot find user. Try again";
 		}
+	} else {
+		$err_msg = "conn error";
 	}
-	echo $err_msg;
+} else {
+	$err_msg = "post error";
 }
-//         $stm = $conn->prepare("SELECT COUNT(*) FROM dbo.UserAccount WHERE Email = :em");
-//         $stm->execute(array(':em' => $email));
-//         $total = $stm->fetchColumn(); 
-//         if ($total == 0) {
-//             die(printf("Cannot find user with email '" .$email. "'."));
-//         }
-//         $stmt = $conn->prepare('SELECT FullName,Email FROM dbo.UserAccount WHERE Email = :email');
-//         try {
-//             $stmt->execute(array('email' => $email));
-//             $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-//             $name = $result['FullName'];
-//         } catch (PDOException $e) {
-//             if ($e->errorInfo[1] == 1054) {
-//                 printf("Cannot find user with email '" .$email. "'.");
-//             } else {
-//                 die(print_r($stmt->errorInfo()));
-//             }
-//         }
-//         $ret = array('email'=>$email,'name'=>$name);
-//         print_r($ret);
-//         print_r(json_encode($result));
-//     }
+echo $err_msg;
 ?>
