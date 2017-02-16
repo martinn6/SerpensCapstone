5711bdacb6d776da4efd4ac5717e7f50be1c33ec
nickdmartin <nickdmartin@outlook.com>
2017-02-15 22:16:45 -0800

    pdf updates v1.9

--- PDFTest.php
+++ PDFTest.php
@@ -1,17 +1,27 @@
 
 <?php
 
-$version = 'v1.7';
+$version = 'v1.9';
+$awardId = htmlspecialchars($_POST["awardId"]);
+$awardedName = "         ";
 
 require_once( "fpdf/fpdf.php" );
 $pdf = new FPDF( 'L', 'mm', 'Letter' );
 $pdf->AddPage();
 $pdf->Image( "images/AwardBackground.png", 0, 0, 280 );
 $pdf->SetFont('Arial','B',48);
-$pdf->Ln(20);
+$pdf->Ln(30);
 $pdf->Cell(0,0,'Employee of the Year',0,1,'C');
 $pdf->SetFont('Arial','B', 8);
-$pdf->Ln(20);
-$pdf->Cell(0,0, $version + "      ",0,1,'R');
+$pdf->Cell(0,0, $version . "      ",0,1,'R');
+
+$pdf->Ln(30);
+$pdf->SetFont('Arial','B',32);
+$pdf->Cell(0,0,'Awarded To',0,1,'C');
+
+$pdf->Ln(30);
+$pdf->SetFont('Arial','U',40);
+$pdf->Cell(0,0,$awardId,0,1,'C');
+
 $pdf->Output("pdftest.pdf","I");
 ?>
