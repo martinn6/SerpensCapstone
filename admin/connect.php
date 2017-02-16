a6bff67fb52a66a1cd208c19d255e2622a44a19d
Maxwell Burdick <Maxwell Burdick>
2017-02-15 18:24:04 -0600

    php stuff

---
+++ admin/connect.php
@@ -0,0 +1,16 @@
+<?php
+	$host = "cs496osusql.database.windows.net";
+	$user = "Serpins_Login";
+	$pwd = "T3amSerpin$!";
+	$db = "OSU_Capstone";
+	try{
+		$conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
+	}
+	catch(Exception $e){
+		die(print_r($e));
+	}
+	
+	$conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+	session_start();
+?>
