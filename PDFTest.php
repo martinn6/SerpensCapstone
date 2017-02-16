83aa60a1fcca6773a6443468675cadd53390e38c
nickdmartin <nickdmartin@outlook.com>
2017-02-15 21:38:31 -0800

    pdf updates v1.0.

--- PDFTest.php
+++ PDFTest.php
@@ -1,9 +1,10 @@
 
 <?php
 require_once( "fpdf/fpdf.php" );
-$pdf = new FPDF( 'P', 'mm', 'A4' );
+$pdf = new FPDF( 'L', 'mm', 'Letter' );
 $pdf->AddPage();
+$pdf->Image( "images/AwardBackground.png", 0, 0, 267 );
 $pdf->SetFont('Arial','B',16);
-$pdf->Cell(40,10,'Hello World!');
+$pdf->Cell(40,10,'Hello World! v1.0');
 $pdf->Output("pdftest.pdf","I");
 ?>
