15073dd762ca5cb9561a81cb56c47d526f0b16b4
Maxwell Burdick <coffeehorn@gmail.com>
2017-02-16 21:05:58 -0600

    a

--- php/addUser.php
+++ php/addUser.php
@@ -1,44 +1,34 @@
 <?php
 
 $email = $_POST["email"];
-$password = $_POST["password"];
-$name = $_POST["FName"] . ' ' . $_POST["LName"];
+$password = md5($_POST["password"]);
+$name = $_POST["FName"];
+require '../php/connect.php';
 
-    // DB connection info
-    $host = "cs496osusql.database.windows.net";
-    $user = "Serpins_Login";
-    $pwd = "T3amSerpin$!";
-    $db = "OSU_Capstone";
-    try{
-        $conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
-    }
-    catch(Exception $e){
-        die(print_r($e));
-    }
-    if ($conn){
-        $stm = $conn->prepare("SELECT COUNT(*) FROM dbo.UserAccount WHERE Email = :em");
-        $stm->execute(array(':em' => $email));
-        $total = $stm->fetch(PDO::FETCH_NUM);
-        if ($total > 0) {
-            print_r($total);
-            die(printf("Cannot add '" .$name. "' because the email '" .$email. "' already exists."));
-        }
-        $stmt = $conn->prepare("INSERT INTO dbo.UserAccount(Email,Password,FullName,UserTypeID)
-            values(:em,:pw,:fn, (SELECT UserTypeId FROM dbo.UserTypes WHERE UserType=:ty))");
-        try {
-             $stmt->execute(array(
-                ':em' => $email,
-                ':pw' => $password,
-                ':fn' => $name,
-                ':ty' => 'Admin'
-            )); 
-        } catch (PDOException $e) {
-            if ($e->errorInfo[1] == 1062) {
-                die(printf("Cannot add '" .$name. "' because the email '" .$email. "' already exists."));
-            } else {
-                die(print_r($stmt->errorInfo()));
-            }
-        }
-        printf("Added '" .$name. "' as an admin user.");
-    }
+if(!empty($_POST)){
+	if ($conn){
+		$query = "SELECT * FROM dbo.UserAccount WHERE Email = :Email";
+		$query_params = array(':Email' => $_POST['email']);
+		$stmt = $conn->prepare($query);
+		$result = $stmt->execute($query_params) or die();
+		$row = $stmt->fetch();
+        
+        	if($row){
+                $err_msg = "Cannot add user because $email already is an admin user.";
+		} else {
+            $query = "INSERT INTO dbo.UserAccount (UserTypeId, Email, FullName, Password) "
+                . "VALUES ((SELECT UserTypeId FROM dbo.UserTypes WHERE UserType='Admin'), :Email, :FullName, :Password)";
+			$query_params = array(
+				':UserTypeId' => 2, 
+				':Email' => $_POST['email'], 
+				':FullName' => $_POST['fname'], 
+				':Password' => $password
+			);
+			$stmt = $conn->prepare($query);
+			$stmt->execute($query_params) or die();
+                
+		}
+	}
+}
+echo $err_msg;
 ?>
