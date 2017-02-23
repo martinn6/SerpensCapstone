9f7bec607571b2aa515dd5cbc504c71734bdb546
nickdmartin <nickdmartin@outlook.com>
2017-02-22 19:14:23 -0800

    EOM award fix

--- php/EOMAward.php
+++ php/EOMAward.php
@@ -14,72 +14,74 @@
 		echo "<p>Award not found.</p>";
 		break;
 	}
-	
-	try{
-		 $conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
-	  }
-	 catch(Exception $e){
-		 die(print_r($e));
-	 }
-	 
-	if($conn)
+	else
 	{
-		$sql_select = 
-			" 	SELECT ag.AwardId, userTo.FullName AS UserToFullname
-				FROM [dbo].[AwardsGiven] AS ag
-				JOIN [dbo].[UserAccount] AS userTo ON userTo.UserId = ag.AwardedToUserId
-				WHERE ag.AwardGivenId = " . (string)$awardGivenId;
+		try{
+			 $conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
+		  }
+		 catch(Exception $e){
+			 die(print_r($e));
+		 }
+		 
+		if($conn)
+		{
+			$sql_select = 
+				" 	SELECT ag.AwardId, userTo.FullName AS UserToFullname
+					FROM [dbo].[AwardsGiven] AS ag
+					JOIN [dbo].[UserAccount] AS userTo ON userTo.UserId = ag.AwardedToUserId
+					WHERE ag.AwardGivenId = " . (string)$awardGivenId;
+					
+			$stmt = $conn->query($sql_select);
+			$awards = $stmt->fetchAll();
+			if(count($awards) > 0) {
+				foreach($awards as $award) {
+					$awardId = $award['AwardId'];
+					$userToFullname = $award['UserToFullname'];
+				}
 				
-		$stmt = $conn->query($sql_select);
-		$awards = $stmt->fetchAll();
-		if(count($awards) > 0) {
-			foreach($awards as $award) {
-				$awardId = $award['AwardId'];
-				$userToFullname = $award['UserToFullname'];
-			}
-			
-			$awardedFrom = "Serpen's Test";
+				$awardedFrom = "Serpen's Test";
 
-			require_once( "../fpdf/fpdf.php" );
-			$pdf = new FPDF( 'L', 'mm', 'Letter' );
-			$pdf->AddPage();
-			$pdf->Image( "../images/EOYAwardBackground.png", 0, 0, 280 );
-			$pdf->Ln(14);
-			$pdf->SetFont('Arial','B', 8);
-			$pdf->Cell(0,0, $version . "           ",0,1,'R');
-			$pdf->Ln(35);
-			$pdf->SetFont('Arial','B',48);
-			$pdf->Cell(0,0,'Employee of the Month',0,1,'C');
+				require_once( "../fpdf/fpdf.php" );
+				$pdf = new FPDF( 'L', 'mm', 'Letter' );
+				$pdf->AddPage();
+				$pdf->Image( "../images/EOYAwardBackground.png", 0, 0, 280 );
+				$pdf->Ln(14);
+				$pdf->SetFont('Arial','B', 8);
+				$pdf->Cell(0,0, $version . "           ",0,1,'R');
+				$pdf->Ln(35);
+				$pdf->SetFont('Arial','B',48);
+				$pdf->Cell(0,0,'Employee of the Month',0,1,'C');
 
-			//Awarded To
-			$pdf->Ln(30);
-			$pdf->SetFont('Arial','B',32);
-			$pdf->Cell(0,0,'Awarded To',0,1,'C');
+				//Awarded To
+				$pdf->Ln(30);
+				$pdf->SetFont('Arial','B',32);
+				$pdf->Cell(0,0,'Awarded To',0,1,'C');
 
-			//Awarded To Name
-			$pdf->Ln(30);
-			$pdf->SetFont('Arial','U',48);
-			$pdf->Cell(0,0,$userToFullname,0,1,'C');
+				//Awarded To Name
+				$pdf->Ln(30);
+				$pdf->SetFont('Arial','U',48);
+				$pdf->Cell(0,0,$userToFullname,0,1,'C');
 
-			//Awarded To Name
-			$pdf->SetXY(24,180);
-			$pdf->SetFont('Arial','B',20);
-			$pdf->Cell(100,0,"From: ____________________",0,0,'L');
-			$pdf->SetX(140);
-			$pdf->Cell(100,0,"Signature: ____________________ ",0,0,'L');
+				//Awarded To Name
+				$pdf->SetXY(24,180);
+				$pdf->SetFont('Arial','B',20);
+				$pdf->Cell(100,0,"From: ____________________",0,0,'L');
+				$pdf->SetX(140);
+				$pdf->Cell(100,0,"Signature: ____________________ ",0,0,'L');
 
-			$pdf->SetXY(47,180);
-			$pdf->Cell(100,0,$awardedFrom,0,0,'L');
+				$pdf->SetXY(47,180);
+				$pdf->Cell(100,0,$awardedFrom,0,0,'L');
 
-			//snake signature
-			$pdf->Image( "../images/SerpensTestSig.png", 175, 160, 80 );
+				//snake signature
+				$pdf->Image( "../images/SerpensTestSig.png", 175, 160, 80 );
 
-			$pdf->Output("EOMAward.pdf","I");
-					
-					
-		}	
-		else {
-			echo "<p>Award not found.</p>";
-		} 
+				$pdf->Output("EOMAward.pdf","I");
+						
+						
+			}	
+			else {
+				echo "<p>Award not found.</p>";
+			} 
+		}
 	}
 ?>
