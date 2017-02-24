b82516d6cfc41a327d293b4d662774d281a7246b
Maxwell Burdick <coffeehorn@gmail.com>
2017-02-24 12:06:34 -0600

    fdsa

--- admin/BIReports.php
+++ admin/BIReports.php
@@ -78,6 +78,7 @@ $(document).ready(function(){
 // 		'&filename='+MyTitle,function(){
 //         document.location.href = '../php/csvTEST.php?timestamp='+MyTimestamp+'&table='+MyTable+'&filename='+MyTitle;
 // 		});
+		
 		var url = "../php/csvTEST.php"
 		var data = {table: MyTable};
 		$.post(url, data, function(result){
@@ -97,6 +98,17 @@ $(document).ready(function(){
 				csv.unshift(fields.join(',')) // add header column
 
 				console.log(csv.join('\r\n'))
+				var link = document.createElement("a");
+            if (link.download !== undefined) { // feature detection
+                // Browsers that support HTML5 download attribute
+                var url = URL.createObjectURL(blob);
+                link.setAttribute("href", url);
+                link.setAttribute("download", filename);
+                link.style.visibility = 'hidden';
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+            }
 // 				var csv = ConvertToCSV(result);
 // 				console.log(csv);
 				$('#success_msg').html("SUCCESS").prop('hidden', false);
