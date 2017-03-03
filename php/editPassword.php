ed077c4c38ce8f60c2e13180d51df11554e0bba2
Maxwell Burdick <Maxwell Burdick>
2017-03-03 06:23:28 -0600

    test

--- php/editPassword.php
+++ php/editPassword.php
@@ -10,13 +10,14 @@ if(!empty($_POST)){
 		$stmt = $conn->prepare($query);
 		$result = $stmt->execute($query_params) or die();
 		$row = $stmt->fetch();
+		$ID = $row['UserId'];
         
         if($row){
                 $new_query = "UPDATE dbo.UserAccount SET Password = :Password
 				WHERE UserId = :ID";
                 $new_query_params = array(
-					':Email' => $newPassword,
-					':ID' => $row['UserId']);
+					':Password' => $newPassword,
+					':ID' => $ID);
                 $new_stmt = $conn->prepare($new_query);
                 $new_result = $new_stmt->execute($new_query_params) or die();
 				if($new_result) {
