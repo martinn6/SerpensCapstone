3259b61ea6aef1dfdb44a416d731de1a120613c8
Maxwell Burdick <Maxwell Burdick>
2017-03-09 09:04:31 -0600

    fads

--- php/biReports.php
+++ php/biReports.php
@@ -26,7 +26,7 @@ if(!empty($_POST)){
 						INNER JOIN 	dbo.AwardGiven on dbo.UserAccount.UserId=dbo.AwardsGiven.AwardedToUserId
 						INNER JOIN 	dbo.Awards on dbo.AwardGiven.AwardId=dbo.Awards.AwardId';
 		} else if ($table == "ABM") {
-			$query = '	SELECT 		datename(month, ag.CreatedDateTime) as Month, count(*) AS "Total",
+			$query = '	SELECT 		count(*) AS "Total",
 					 	FROM 		[dbo].[AwardsGiven] AS ag
 					  	GROUP BY 	datename(month, ag.CreatedDateTime)
 						  ';
