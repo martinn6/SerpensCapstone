0a5f407f2df02fc27c1e573109713765340b52dc
Maxwell Burdick <Maxwell Burdick>
2017-03-09 06:14:48 -0600

    isActive

--- php/deleteUser.php
+++ php/deleteUser.php
@@ -3,7 +3,7 @@ $email = $_POST["email"];
 require '../php/connect.php';
 if(!empty($_POST)){
 	if ($conn){
-		$query = "SELECT * FROM dbo.UserAccount WHERE Email = :Email AND 
+		$query = "SELECT * FROM dbo.UserAccount WHERE IsActive = 1 AND Email = :Email AND 
 		UserTypeId = (SELECT UserTypeId FROM dbo.UserTypes WHERE UserType='Admin')";
 		$query_params = array(':Email' => $_POST['email']);
 		$stmt = $conn->prepare($query);
