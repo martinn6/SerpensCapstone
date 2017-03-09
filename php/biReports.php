f409ad4605c9f3486a83dda58c9775c6c85bba62
Maxwell Burdick <Maxwell Burdick>
2017-03-09 13:48:19 -0600

    asfd

--- php/biReports.php
+++ php/biReports.php
@@ -8,25 +8,29 @@ if(!empty($_POST)){
 			$query = '	SELECT 		* 
 						FROM 		dbo.UserAccount';
 		} else if ($table == "UBT") {
-			$query = '	SELECT 		ua.UserType as "Type", count(*) as "Count" 
+			$query = '	SELECT 		ut.UserType as "Type", 
+									count(*) as "Count" 
 					  	FROM 		[dbo].[AwardsGiven] AS ag
 						JOIN 		[dbo].[UserAccount] AS ua ON ua.UserID = ag.AwardedGivenByUserId
 						JOIN 		[dbo].[UserTypes] AS ut ON ut.TypeId = ua.TypeId
-						GROUP BY 	ua.TypeId
+						GROUP BY 	ut.UserType
 						';
 		} else if ($table == "ABUG") {
-			$query = '	SELECT 		ua.FullName as "Name", count(*) as "Count" 
+			$query = '	SELECT 		ua.FullName as "Name", 
+									count(*) as "Count" 
 					  	FROM 		[dbo].[AwardsGiven] AS ag
 						JOIN 		[dbo].[UserAccount] AS ua ON ua.UserID = ag.AwardedGivenByUserId
 						GROUP BY 	ua.FullName
 						';
 		} else if ($table == "ABM") {
-			$query = '	SELECT 		Month = datename(m, ag.CreatedDateTime), count(*) AS "Total"
+			$query = '	SELECT 		Month = datename(m, ag.CreatedDateTime), 
+									count(*) AS "Total"
   					 	FROM 		[dbo].[AwardsGiven] AS ag
   					  	GROUP BY 	datename(m, ag.CreatedDateTime)
 						';
 		} else if ($table == "ABT") {
-			$query = '	SELECT 		aws.AwardTypeName AS "Award", count(*) AS "Count"
+			$query = '	SELECT 		aws.AwardTypeName AS "Award", 
+									count(*) AS "Count"
 					 	FROM 		[dbo].[AwardsGiven] AS ag
 						JOIN 		[dbo].[Awards] AS aws ON aws.AwardId = ag.AwardId
 					  	GROUP BY 	aws.AwardTypeName
