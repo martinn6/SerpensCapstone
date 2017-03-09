7e6fcf05cb4c8dfd46d24e21408a67988c541c81
Maxwell Burdick <Maxwell Burdick>
2017-03-09 06:28:16 -0600

    fas

--- admin/BIReports.php
+++ admin/BIReports.php
@@ -121,7 +121,7 @@ $(document).ready(function(){
 	$("#ABTCSV").click(function(e){
 		e.preventDefault();
 		var MyTable = "ABM";
-  		var filename = "AwardsByMonth.csv";		
+  		var filename = "AwardsByType.csv";		
 		var url = "../php/biReports.php"
 		var data = {table: MyTable};
 		$.post(url, data, function(result){
