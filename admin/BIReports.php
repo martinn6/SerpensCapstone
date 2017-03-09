41a767743e4f306d9a10149194c7d744e2943190
Maxwell Burdick <Maxwell Burdick>
2017-03-09 13:39:36 -0600

    bi changes

--- admin/BIReports.php
+++ admin/BIReports.php
@@ -81,6 +81,155 @@ function ConvertToCSV(json, filename) {
 	    }
 	}
 }
+
+function ABUGChart(){
+	// set the dimensions of the canvas
+	var margin = {top: 20, right: 20, bottom: 70, left: 40},
+		width = 600 - margin.left - margin.right,
+		height = 300 - margin.top - margin.bottom;
+
+
+	// set the ranges
+	var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
+
+	var y = d3.scale.linear().range([height, 0]);
+
+	// define the axis
+	var xAxis = d3.svg.axis()
+		.scale(x)
+		.orient("bottom")
+
+
+	var yAxis = d3.svg.axis()
+		.scale(y)
+		.orient("left")
+		.ticks(10);
+
+
+	// add the SVG element
+	var svg = d3.select("#ABUG-chart").append("svg")
+		.attr("width", width + margin.left + margin.right)
+		.attr("height", height + margin.top + margin.bottom)
+		.append("g")
+		.attr("transform", 
+			"translate(" + margin.left + "," + margin.top + ")");
+
+		var url = "../php/biReports.php";
+		var table = "ABUG";
+		var send = {table: table};
+		$.post(url, send, function(data){
+
+			data.forEach(function(d) {
+				d.User = d.User;
+				d.Count = +d.Count;
+			});
+			
+		// scale the range of the data
+		x.domain(data.map(function(d) { return d.User; }));
+		y.domain([0, d3.max(data, function(d) { return d.Count; })]);      
+
+ 		svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+		svg.append("g")
+			.attr("class", "y axis")
+			.call(yAxis)
+			.append("text")
+			.attr("transform", "rotate(-90)")
+			.attr("y", 5)
+			.attr("dy", ".71em")
+			.style("text-anchor", "end")
+			.text("Total");
+
+
+		// Add bar chart
+		svg.selectAll("bar")
+			.data(data)
+			.enter().append("rect")
+			.attr("class", "bar")
+			.attr("x", function(d) { return x(d.User); })
+			.attr("width", x.rangeBand())
+			.attr("y", function(d) { return y(d.Count); })
+			.attr("height", function(d) { return height - y(d.Count); });
+	});
+}
+
+function UBTChart(){
+	// set the dimensions of the canvas
+	var margin = {top: 20, right: 20, bottom: 70, left: 40},
+		width = 600 - margin.left - margin.right,
+		height = 300 - margin.top - margin.bottom;
+
+
+	// set the ranges
+	var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
+
+	var y = d3.scale.linear().range([height, 0]);
+
+	// define the axis
+	var xAxis = d3.svg.axis()
+		.scale(x)
+		.orient("bottom")
+
+
+	var yAxis = d3.svg.axis()
+		.scale(y)
+		.orient("left")
+		.ticks(10);
+
+
+	// add the SVG element
+	var svg = d3.select("#UBT-chart").append("svg")
+		.attr("width", width + margin.left + margin.right)
+		.attr("height", height + margin.top + margin.bottom)
+		.append("g")
+		.attr("transform", 
+			"translate(" + margin.left + "," + margin.top + ")");
+
+		var url = "../php/biReports.php";
+		var table = "UBT";
+		var send = {table: table};
+		$.post(url, send, function(data){
+
+			data.forEach(function(d) {
+				d.Type = d.Type;
+				d.Count = +d.Count;
+			});
+			
+		// scale the range of the data
+		x.domain(data.map(function(d) { return d.Type; }));
+		y.domain([0, d3.max(data, function(d) { return d.Count; })]);      
+
+ 		svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+		svg.append("g")
+			.attr("class", "y axis")
+			.call(yAxis)
+			.append("text")
+			.attr("transform", "rotate(-90)")
+			.attr("y", 5)
+			.attr("dy", ".71em")
+			.style("text-anchor", "end")
+			.text("Total");
+
+
+		// Add bar chart
+		svg.selectAll("bar")
+			.data(data)
+			.enter().append("rect")
+			.attr("class", "bar")
+			.attr("x", function(d) { return x(d.Type); })
+			.attr("width", x.rangeBand())
+			.attr("y", function(d) { return y(d.Count); })
+			.attr("height", function(d) { return height - y(d.Count); });
+	});
+}
+
 function ABMChart(){
 	// set the dimensions of the canvas
 	var margin = {top: 20, right: 20, bottom: 70, left: 40},
@@ -126,17 +275,6 @@ function ABMChart(){
 		// scale the range of the data
 		x.domain(data.map(function(d) { return d.Month; }));
 		y.domain([0, d3.max(data, function(d) { return d.Total; })]);      
-
-				// add axis
-		// svg.append("g")
-		// 	.attr("class", "x axis")
-		// 	.attr("transform", "translate(0," + height + ")")
-		// 	.call(xAxis)
-		// 	.selectAll("text")
-		// 	.style("text-anchor", "end")
-		// 	.attr("dx", "-.8em")
-		// 	.attr("dy", "-.55em")
-		// 	.attr("transform", "rotate(-0)" );
 
  		svg.append("g")
             .attr("class", "x axis")
@@ -212,17 +350,6 @@ function ABTChart(){
 		x.domain(data.map(function(d) { return d.Award; }));
 		y.domain([0, d3.max(data, function(d) { return d.Count; })]);      
 
-				// add axis
-		// svg.append("g")
-		// 	.attr("class", "x axis")
-		// 	.attr("transform", "translate(0," + height + ")")
-		// 	.call(xAxis)
-		// 	.selectAll("text") 
-		// 	.style("text-anchor", "end")
-		// 	.attr("dx", "-.8em")
-		// 	.attr("dy", "-.55em")
-		// 	.attr("transform", "rotate(-90)" );
-
 		svg.append("g")
             .attr("class", "x axis")
             .attr("transform", "translate(0," + height + ")")
@@ -269,10 +396,10 @@ $(document).ready(function(){
 				}
 			});
 	});
-	$("#EOMawardsCSV").click(function(e){
+	$("#ABUGCSV").click(function(e){
 		e.preventDefault();
-		var MyTable = "EOM";
-		var filename = "EOM_Awards.csv";
+		var MyTable = "ABUG";
+		var filename = "AwardsGivenByUser.csv";
 		var url = "../php/biReports.php"
 		var data = {table: MyTable};
 		$.post(url, data, function(result){
@@ -284,10 +411,10 @@ $(document).ready(function(){
 			}
 		});
 	});
-	$("#usersCSV").click(function(e){
+	$("#UBTCSV").click(function(e){
 		e.preventDefault();
-		var MyTable = "users";
-  		var filename = "UserAccounts.csv";		
+		var MyTable = "UBTCSV";
+  		var filename = "UsersByType.csv";		
 		var url = "../php/biReports.php"
 		var data = {table: MyTable};
 		$.post(url, data, function(result){
@@ -348,95 +475,111 @@ $(document).ready(function(){
 	</div>
 </nav>
 <div class='alert alert-danger' id="error_msg" hidden></div>
+<div class='alert alert-success' id="success_msg" hidden></div>	
 <div class="container">
 	<div class="row">
-		<section class="col-xs-offset-1 col-xs-6">
-			<h2><?php echo $user; ?> Business Intelligence Reports</h2>
+		<section class="col-xs-offset-1 col-xs-10">
+			<h1><?php echo $user; ?> Business Intelligence Reports</h1>
 		</section>
 	</div>
 
-	<div class="panel panel-default">
-		<div class="panel-heading">
-			<h4 class="panel-title">
-				<a data-toggle="collapse" data-parent="#accordion" href="#collapsePassword">
-				EOM Awards Report</a>
-			</h4>
-		</div>
-	<div id="collapsePassword" class="panel-collapse collapse">
-	<div class="row">
+    <div class="row">
 		<section class="col-xs-offset-2 col-xs-8">
-			<form class="form-horizontal" >
-				<div class="row">
-					<div class="form-group">
-						<!--<label class="col-sm-2" for="report Type">
-						EOM Awards Report
-						</label>-->
-						<div class="col-sm-10">
-						<button type="submit" id="EOMawardsCSV"
-						 class="btn btn-default">Download CSV</button>
-						</div>
+			<div class="panel-group" id="accordion">
+
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h4 class="panel-title">
+							<a data-toggle="collapse" data-parent="#accordion" href="#collapseABT">
+							Total Awards By Type</a>
+						</h4>
 					</div>
-				</div>
-				<div class="row">
-					<div class="form-group">
-						<label class="col-sm-2" for="report Type">
-						EOY Awards Report
-						</label>
-						<div class="col-sm-10">
-						<button type="submit" id="EOYawardsCSV"
-						 class="btn btn-default">Download CSV</button>
+					<div id="collapseABT" class="panel-collapse collapse">
+						<div class="panel-body">
+							<div class="col-sm-10">
+						        <div id="ABT-chart">
+							        <script type="text/javascript">
+								        ABTChart();
+							        </script>
+						        </div>
+						        <button type="submit" id="ABTCSV"
+                                class="btn btn-default">Download CSV</button>
+                            </div>
 						</div>
+					</div> 
+				</div>
+
+                <div class="panel panel-default">
+					<div class="panel-heading">
+						<h4 class="panel-title">
+							<a data-toggle="collapse" data-parent="#accordion" href="#collapseABM">
+							Total Awards By Month</a>
+						</h4>
 					</div>
+					<div id="collapseABM" class="panel-collapse collapse">
+						<div class="panel-body">
+							<div class="col-sm-10">
+						        <div id="ABM-chart">
+							        <script type="text/javascript">
+								        ABMChart();
+							        </script>
+						        </div>
+						        <button type="submit" id="ABMCSV"
+                                class="btn btn-default">Download CSV</button>
+                            </div>
+						</div>
+					</div> 
 				</div>
-				<div class="row">
-					<div class="form-group">
-						<label class="col-sm-2" for="report Type">
-						Awards By Month
-						</label>
-						<div class="col-sm-10">
-							<div id="ABM-chart">
-							<script type="text/javascript">
-								ABMChart();
-							</script>
-						</div>
-						<button type="submit" id="ABMCSV"
-						 class="btn btn-default">Download CSV</button>
-						</div>
+				
+                <div class="panel panel-default">
+					<div class="panel-heading">
+						<h4 class="panel-title">
+							<a data-toggle="collapse" data-parent="#accordion" href="#collapseABUG">
+							Awards Given by User</a>
+						</h4>
 					</div>
+					<div id="collapseABUG" class="panel-collapse collapse">
+						<div class="panel-body">
+							<div class="col-sm-10">
+						        <div id="ABUG-chart">
+							        <script type="text/javascript">
+								        ABUGChart();
+							        </script>
+						        </div>
+						        <button type="submit" id="ABUGCSV"
+                                class="btn btn-default">Download CSV</button>
+                            </div>
+						</div>
+					</div> 
 				</div>
-				<div class="row">
-					<div class="form-group">
-						<label class="col-sm-2" for="report Type">
-						Awards By Type
-						</label>
-						<div class="col-sm-10">
-						<div id="ABT-chart">
-							<script type="text/javascript">
-								ABTChart();
-							</script>
-						</div>
-						<button type="submit" id="ABTCSV"
-						 class="btn btn-default">Download CSV</button>
-						</div>
+				
+                                <div class="panel panel-default">
+					<div class="panel-heading">
+						<h4 class="panel-title">
+							<a data-toggle="collapse" data-parent="#accordion" href="#collapseUBT">
+							Users By Type</a>
+						</h4>
 					</div>
-				</div>
-				<div class="row">
-					<div class="form-group">
-						<label class="col-sm-2" for="report Type">
-						Users Report
-						</label>
-						<div class="col-sm-10">
-						<button type="submit" id="usersCSV"
-						 class="btn btn-default">Download CSV</button>
+					<div id="collapseUBT" class="panel-collapse collapse">
+						<div class="panel-body">
+							<div class="col-sm-10">
+						        <div id="UBT-chart">
+							        <script type="text/javascript">
+								        UBTChart();
+							        </script>
+						        </div>
+						        <button type="submit" id="UBTCSV"
+                                class="btn btn-default">Download CSV</button>
+                            </div>
 						</div>
-					</div>
+					</div> 
 				</div>
-			</form>
-		</section>
-	</div>
-	
+				
+			</div>
+		</div>
+	</section>
 	<div class="row">
-		<section class="col-xs-offset-3 col-xs-6">
+		<section class="col-xs-offset-5 col-xs-2">
 			<a href="admin.php" role="button" class="btn btn-primary">Return to Admin</a>
 		</section>
 	</div>
--- php/biReports.php
+++ php/biReports.php
@@ -7,53 +7,32 @@ if(!empty($_POST)){
 		if ($table == "users") {
 			$query = '	SELECT 		* 
 						FROM 		dbo.UserAccount';
-		} else if ($table == "EOM") {
-			$query = '	SELECT 		dbo.UserAccount.FullName, 
-									dbo.Awards.AwardTypeName,
-									dbo.AwardsGiven.AwardedDate,
-									dbo.AwardsGiven.AwardURL
-					  	FROM 		dbo.UserAccount
-						WHERE 		dbo.Awards.AwardTypeName = "Employee of the Month"
-						JOIN		dbo.AwardsGiven on dbo.UserAccount.UserId = dbo.AwardsGiven.AwardedToUserId
-						JOIN 		dbo.Awards on dbo.AwardGiven.AwardId = dbo.Awards.AwardId';
-		} else if ($table == "EOY") {
-			$query = '	SELECT 		dbo.UserAccount.FullName, 
-									dbo.Awards.AwardTypeName,
-									dbo.AwardsGiven.AwardedDate,
-									dbo.AwardsGiven.AwardURL
-						FROM 		dbo.UserAccount
-						WHERE 		dbo.Awards.AwardTypeName = "Employee of the Month"
-						INNER JOIN 	dbo.AwardGiven on dbo.UserAccount.UserId=dbo.AwardsGiven.AwardedToUserId
-						INNER JOIN 	dbo.Awards on dbo.AwardGiven.AwardId=dbo.Awards.AwardId';
+		} else if ($table == "UBT") {
+			$query = '	SELECT 		ua.UserType as "Type", count(*) as "Count" 
+					  	FROM 		[dbo].[UserAccount] AS ua
+						JOIN 		[dbo].[UserTypes] AS ut ON ut.TypeId = ua.TypeId
+						JOIN 		[dbo].[AwardsGiven] AS ag ON ag.AwardedGivenByUserId = ua.UserId
+						GROUP BY 	ua.TypeId
+						';
+		} else if ($table == "ABUG") {
+			$query = '	SELECT 		ua.FullName as "Name", count(*) as "Count" 
+					  	FROM 		[dbo].[UserAccount] AS ua
+						JOIN 		[dbo].[UserTypes] AS ut ON ut.TypeId = ua.TypeId
+						JOIN 		[dbo].[AwardsGiven] AS ag ON ag.AwardedGivenByUserId = ua.UserId
+						GROUP BY 	ua.FullName
+						';
 		} else if ($table == "ABM") {
 			$query = '	SELECT 		Month = datename(m, ag.CreatedDateTime), count(*) AS "Total"
   					 	FROM 		[dbo].[AwardsGiven] AS ag
   					  	GROUP BY 	datename(m, ag.CreatedDateTime)
-						  ';
+						';
 		} else if ($table == "ABT") {
 			$query = '	SELECT 		aws.AwardTypeName AS "Award", count(*) AS "Count"
 					 	FROM 		[dbo].[AwardsGiven] AS ag
 						JOIN 		[dbo].[Awards] AS aws ON aws.AwardId = ag.AwardId
 					  	GROUP BY 	aws.AwardTypeName
-						  ';
-		} else if ($table == "Atest") {
-			$query = 'count(ag.AwardId) as "Total",
-	Count(CASE datepart(m, ag.CreatedDateTime) WHEN 1 THEN 1 ELSE NULL END) AS Jan,
-    Count(CASE datepart(m, ag.CreatedDateTime) WHEN 2 THEN 1 ELSE NULL END) AS Feb,
-    Count(CASE datepart(m, ag.CreatedDateTime) WHEN 3 THEN 1 ELSE NULL END) AS Mar,
-    Count(CASE datepart(m, ag.CreatedDateTime) WHEN 4 THEN 1 ELSE NULL END) AS Apr,
-    Count(CASE datepart(m, ag.CreatedDateTime) WHEN 5 THEN 1 ELSE NULL END) AS May,
-    Count(CASE datepart(m, ag.CreatedDateTime) WHEN 6 THEN 1 ELSE NULL END) AS Jun,
-    Count(CASE datepart(m, ag.CreatedDateTime) WHEN 7 THEN 1 ELSE NULL END) AS Jul,
-    Count(CASE datepart(m, ag.CreatedDateTime) WHEN 8 THEN 1 ELSE NULL END) AS Aug,
-    Count(CASE datepart(m, ag.CreatedDateTime) WHEN 9 THEN 1 ELSE NULL END) AS Sep,
-    Count(CASE datepart(m, ag.CreatedDateTime) WHEN 10 THEN 1 ELSE NULL END) AS Oct,
-    Count(CASE datepart(m, ag.CreatedDateTime) WHEN 11 THEN 1 ELSE NULL END) AS Nov,
-    Count(CASE datepart(m, ag.CreatedDateTime) WHEN 12 THEN 1 ELSE NULL END) AS Dec
-	FROM dbo].[AwardsGiven] AS ag
-	GROUP BY 	Total
-						  ';
-		}
+						';
+		} 
 		
 	
 		$stmt = $conn->prepare($query);
