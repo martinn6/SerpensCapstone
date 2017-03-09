85c6e6a83a9f399ffe34e70e79f1d5c0824a0135
Maxwell Burdick <Maxwell Burdick>
2017-03-09 10:55:54 -0600

    afsd

--- admin/BIReports.php
+++ admin/BIReports.php
@@ -83,80 +83,70 @@ function ConvertToCSV(json, filename) {
 }
 
 function ABTChart(){
-	// set the dimensions of the canvas
-	var margin = {top: 20, right: 20, bottom: 70, left: 40},
-		width = 600 - margin.left - margin.right,
-		height = 300 - margin.top - margin.bottom;
-
-
-	// set the ranges
-	var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
-
-	var y = d3.scale.linear().range([height, 0]);
-
-	// define the axis
-	var xAxis = d3.svg.axis()
-		.scale(x)
-		.orient("bottom")
-
-
-	var yAxis = d3.svg.axis()
-		.scale(y)
-		.orient("left")
-		.ticks(10);
-
-
-	// add the SVG element
-	var svg = d3.select("ABT-chart").append("svg")
-		.attr("width", width + margin.left + margin.right)
-		.attr("height", height + margin.top + margin.bottom)
-		.append("g")
-		.attr("transform", 
-			"translate(" + margin.left + "," + margin.top + ")");
+	
 	var url = "../php/biReports.php";
 	var table = "ABT";
 	var send = {table: table};
 	$.post(url, send, function(data){
-		data.forEach(function(d) {
-        d.Award = d.Award;
-        d.count = +d.Count;
-    });
-	
-  // scale the range of the data
-  x.domain(data.map(function(d) { return d.Award; }));
-  y.domain([0, d3.max(data, function(d) { return d.Count; })]);
+		var margin = {top: 20, right: 20, bottom: 70, left: 40},
+    		width = 600 - margin.left - margin.right,
+    		height = 300 - margin.top - margin.bottom;
+		
+		//x and y Scales
+		var xScale = d3.scale.ordinal()
+			.rangeRoundBands([0, width], .1);
 
-  // add axis
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
-    .selectAll("text")
-      .style("text-anchor", "end")
-      .attr("dx", "-.8em")
-      .attr("dy", "-.55em")
-      .attr("transform", "rotate(-90)" );
+		var yScale = d3.scale.linear()
+			.range([height, 0]);
 
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 5)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Frequency");
+		xScale.domain(data.map(function(d) { return d.Award; }));
+		yScale.domain([0, d3.max(data, function(d) { return d.Count; })]);
 
+		//x and y Axes
+		var xAxis = d3.svg.axis()
+			.scale(xScale)
+			.orient("bottom");
 
-  // Add bar chart
-  svg.selectAll("bar")
-      .data(data)
-    .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d) { return x(d.Letter); })
-      .attr("width", x.rangeBand())
-      .attr("y", function(d) { return y(d.Freq); })
-      .attr("height", function(d) { return height - y(d.Freq); });
+		var yAxis = d3.svg.axis()
+			.scale(yScale)
+			.orient("left")
+			.ticks(5);
+
+		//create svg container
+		var svg = d3.select("#ABT-chart")
+			.append("svg")
+			.attr("width", width + margin.left + margin.right)
+			.attr("height", height + margin.top + margin.bottom)
+			.append("g")
+			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");        
+
+		//create bars
+		svg.selectAll(".bar")
+			.data(data)
+			.enter()
+			.append("rect")
+			.attr("class", "bar")
+			.attr("x", function(d) { return xScale(d.x); })
+			.attr("width", xScale.rangeBand())
+			.attr("y", function(d) { return yScale(d.y); })
+			.attr("height", function(d) { return height - yScale(d.y); });
+
+		//drawing the x axis on svg
+		svg.append("g")
+			.attr("class", "x axis")
+			.attr("transform", "translate(0," + height + ")")
+			.call(xAxis);
+
+		//drawing the y axis on svg
+		svg.append("g")
+			.attr("class", "y axis")
+			.call(yAxis)
+			.append("text")
+			.attr("transform", "rotate(-90)")
+			.attr("y", 6)
+			.attr("dy", ".71em")
+			.style("text-anchor", "end")
+			.text("Count");
 	});
 }
 
