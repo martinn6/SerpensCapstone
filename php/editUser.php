7d1366881ac4680fe488e61b00f94ffe7cf92827
Maxwell Burdick <Maxwell Burdick>
2017-03-10 17:28:02 -0600

    allow for editing all user ypes

--- php/editUser.php
+++ php/editUser.php
@@ -5,8 +5,7 @@ require '../php/connect.php';
 
 if(!empty($_POST)){
 	if ($conn){
-		$query = "SELECT * FROM dbo.UserAccount WHERE IsActive = 1 AND Email = :Email AND 
-		UserTypeId = (SELECT UserTypeId FROM dbo.UserTypes WHERE UserType='Admin')";
+		$query = "SELECT * FROM dbo.UserAccount WHERE IsActive = 1 AND Email = :Email";
 		$query_params = array(':Email' => $_POST['email']);
 		$stmt = $conn->prepare($query);
 		$result = $stmt->execute($query_params) or die();
