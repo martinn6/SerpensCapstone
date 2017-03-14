25cb4e5337c7e37f65f766f729dca962ac803fad
Maxwell Burdick <Maxwell Burdick>
2017-03-14 18:41:39 -0500

    test

--- php/biReports.php
+++ php/biReports.php
@@ -43,11 +43,11 @@ if(!empty($_POST)){
 		// 				GROUP BY 	ua.FullName
 		// 				';
 		} else if ($table == "ABM") {
-			$query = '	SELECT 		CONVERT(varchar(2),  ag.AwardedDate, 101) as "Month",
+			$query = '	SELECT 		CONVERT(varchar(2),  ag.AwardedDate, 101)+CONVERT(varchar(3),  ag.AwardedDate, 0) as "Month",
 									count(*) AS "Total"
   					 	FROM 		[dbo].[AwardsGiven] AS ag
 						WHERE		ag.IsDeleted = 0
-						GROUP BY	"Month"
+						GROUP BY	CONVERT(varchar(2),  ag.AwardedDate, 101)
 						';
 		// } else if ($table == "ABMforCSV") {
 		// 	$query = '	SELECT 		"Award Month" = datename(m, ag.AwardedDate),  
