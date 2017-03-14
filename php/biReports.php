86601c88291c98f32348364be41167c8a364ad53
Maxwell Burdick <coffeehorn@gmail.com>
2017-03-14 07:45:52 -0500

    test

--- php/biReports.php
+++ php/biReports.php
@@ -48,7 +48,7 @@ if(!empty($_POST)){
 									count(*) AS "Total"
   					 	FROM 		[dbo].[AwardsGiven] AS ag
 						WHERE		ag.IsDeleted = 0
-  					  	GROUP BY 	ag.AwardedDate
+  					  	GROUP BY 	"MInt"
 						ORDER BY 	"MInt"
 						';
 		// } else if ($table == "ABMforCSV") {
