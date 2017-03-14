04658030e7afc4aa5b48b7377b4fb3cf4b7352ed
Maxwell Burdick <Maxwell Burdick>
2017-03-14 06:22:40 -0500

    test

--- php/biReports.php
+++ php/biReports.php
@@ -47,7 +47,7 @@ if(!empty($_POST)){
 									count(*) AS "Total"
   					 	FROM 		[dbo].[AwardsGiven] AS ag
 						WHERE		ag.IsDeleted = 0
-  					  	GROUP BY 	Month
+  					  	GROUP BY 	ag.AwardedDate
 						-- ORDER BY 	datepart(m, ag.AwardedDate)
 						';
 		// } else if ($table == "ABMforCSV") {
