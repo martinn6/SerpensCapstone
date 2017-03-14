1eebf178ac03c822befccda6d230c2473c8bbc61
Maxwell Burdick <Maxwell Burdick>
2017-03-14 18:22:33 -0500

    test

--- admin/BIReports.php
+++ admin/BIReports.php
@@ -57,7 +57,7 @@ $user = $_SESSION['admin']['name'];
     display: inline-block;
     position: relative;
     width: 100%;
-    padding-bottom: 100%;
+    padding-bottom: 50%;
     vertical-align: top;
     overflow: hidden;
 }
