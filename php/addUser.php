20b0dc11a132adc5a7840c7de7c0438ba2d24608
Maxwell Burdick <Maxwell Burdick>
2017-03-14 17:18:18 -0500

    query update

--- php/addUser.php
+++ php/addUser.php
@@ -7,8 +7,7 @@ require '../php/connect.php';
 
 if(!empty($_POST)){
 	if ($conn){
-		$query = "SELECT * FROM dbo.UserAccount WHERE IsActive = 1 AND Email = :Email
-		UserTypeId = (SELECT UserTypeId FROM dbo.UserTypes WHERE UserType='Admin')";
+		$query = "SELECT * FROM dbo.UserAccount WHERE IsActive = 1 AND Email = :Email";
 		$query_params = array(':Email' => $_POST['email']);
 		$stmt = $conn->prepare($query);
 		$result = $stmt->execute($query_params) or die();
